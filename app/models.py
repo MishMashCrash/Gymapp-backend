477a0f970b10f69d4eecd2511c8d08a3320ea2e5
LosingGround <majd.alashqar@mail.utoronto.ca>
2026-09-11 22:57:56 -0400

    add users table

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
 from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
