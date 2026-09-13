146b296a5b0d54821feebbcf94fb3ab4a4a24105
LosingGround <majd.alashqar@mail.utoronto.ca>
2026-09-12 21:07:40 -0400

    add is_admin to users table

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    is_admin = Column(Boolean, nullable=False, server_default="false")
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
