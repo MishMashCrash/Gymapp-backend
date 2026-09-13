b6df39f75aa5838d17db5a948a50d5245a0542a1
LosingGround <majd.alashqar@mail.utoronto.ca>
2026-09-13 18:56:36 -0400

    add workoutset table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text, ForeignKey
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    TIMESTAMP,
+    text,
+    ForeignKey,
+    Float,
+)
 from sqlalchemy.orm import relationship
 
 
@@ -13,6 +22,7 @@ class User(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
+
 
 class Exercise(Base):
     __tablename__ = "exercises"
@@ -32,18 +42,24 @@ class Exercise(Base):
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
 
+
 class Split(Base):
     __tablename__ = "splits"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     owner = relationship("User")
     description = Column(String)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
-    days = relationship("SplitDay", back_populates="split", cascade="all, delete-orphan")
+    days = relationship(
+        "SplitDay", back_populates="split", cascade="all, delete-orphan"
+    )
+
 
 class SplitDay(Base):
     __tablename__ = "split_days"
@@ -51,9 +67,13 @@ class SplitDay(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
     order = Column(Integer, nullable=False)
-    split_id = Column(Integer, ForeignKey("splits.id", ondelete="CASCADE"), nullable=False)
+    split_id = Column(
+        Integer, ForeignKey("splits.id", ondelete="CASCADE"), nullable=False
+    )
     split = relationship("Split", back_populates="days")
-    exercises = relationship("SplitDayExercise", back_populates="split_day", cascade="all, delete-orphan")
+    exercises = relationship(
+        "SplitDayExercise", back_populates="split_day", cascade="all, delete-orphan"
+    )
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
@@ -63,8 +83,12 @@ class SplitDayExercise(Base):
     __tablename__ = "split_day_exercises"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    split_day_id = Column(Integer, ForeignKey("split_days.id", ondelete="CASCADE"), nullable=False)
-    exercise_id = Column(Integer, ForeignKey("exercises.id", ondelete="CASCADE"), nullable=False)
+    split_day_id = Column(
+        Integer, ForeignKey("split_days.id", ondelete="CASCADE"), nullable=False
+    )
+    exercise_id = Column(
+        Integer, ForeignKey("exercises.id", ondelete="CASCADE"), nullable=False
+    )
     order = Column(Integer, nullable=False)
     target_sets = Column(Integer)
     target_reps = Column(String)
@@ -75,14 +99,43 @@ class SplitDayExercise(Base):
     )
     exercise = relationship("Exercise")
 
-class Workoutout(Base):
+
+class Workout(Base):
     __tablename__ = "workouts"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    split_day_id = Column(Integer, ForeignKey("split_days.id", ondelete="CASCADE"), nullable=True)
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    date = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    split_day_id = Column(
+        Integer, ForeignKey("split_days.id", ondelete="CASCADE"), nullable=True
+    )
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    date = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
     notes = Column(String)
 
     split_day = relationship("SplitDay")
+    sets = relationship(
+        "WorkoutSet", back_populates="workout", cascade="all, delete-orphan"
+    )
     owner = relationship("User")
+
+
+class WorkoutSet(Base):
+    __tablename__ = "workout_sets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    workout_id = Column(
+        Integer, ForeignKey("workouts.id", ondelete="CASCADE"), nullable=False
+    )
+    exercise_id = Column(
+        Integer, ForeignKey("exercises.id", ondelete="CASCADE"), nullable=False
+    )
+    set_number = Column(Integer, nullable=False)
+    reps = Column(Integer, nullable=False)
+    weight = Column(Float, nullable=False)
+    rpe = Column(Integer)
+
+    workout = relationship("Workout", back_populates="sets")
+    exercise = relationship("Exercise")
