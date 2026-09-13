3bf1b71ea9356d61a4185f3e121fe47782197f3a
LosingGround <majd.alashqar@mail.utoronto.ca>
2026-09-12 22:36:16 -0400

    add get_single_exercise_by_ID endpoint, refractor outh2 to ouath2

--- app/routers/exercise.py
+++ app/routers/exercise.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from .. import schemas, database, models, utils, outh2
+from .. import ouath2, schemas, database, models, utils
 from fastapi.security.oauth2 import OAuth2PasswordRequestFormStrict
 from typing import List
 from sqlalchemy import or_
@@ -11,9 +11,9 @@ router = APIRouter(prefix="/exercise", tags=["Exercises"])
 @router.get("/", response_model=List[schemas.ExerciseOut])
 def get_exercises(
     db: Session = Depends(database.get_db),
-    current_user: models.User = Depends(outh2.get_current_user),
+    current_user: models.User = Depends(ouath2.get_current_user),
 ):
-    results = (
+    exercises = (
         db.query(models.Exercise)
         .filter(
             or_(
@@ -22,6 +22,32 @@ def get_exercises(
             )
         )
         .all()
-    
     )
-    return results
+    return exercises
+
+
+@router.get("/{id}", response_model=schemas.ExerciseOut)
+def get_exercise_by_ID(
+    id: int,
+    db: Session = Depends(database.get_db),
+    current_user: models.User = Depends(ouath2.get_current_user),
+):
+    exercise = (
+        db.query(models.Exercise)
+        .filter(
+            models.Exercise.id == id,
+            or_(
+                models.Exercise.owner_id == current_user.id,
+                models.Exercise.type == "staple",
+            ),
+        )
+        .first()
+    )
+
+    if exercise is not None:
+        return exercise
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"exercise with id: {id} was not found",
+        )
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from .. import schemas, database, models, utils, outh2
+from .. import ouath2, schemas, database, models, utils
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
